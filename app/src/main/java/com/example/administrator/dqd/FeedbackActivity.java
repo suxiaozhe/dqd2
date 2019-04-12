92c76505ebaef8466d5c24fdd645582e3460411f
suxiaozhe <1716800825@qq.com>
2019-04-12 08:46:05 +0800

    添加文件4.12个人中心加入右滑退出

--- app/src/main/java/com/example/administrator/dqd/FeedbackActivity.java
+++ app/src/main/java/com/example/administrator/dqd/FeedbackActivity.java
@@ -3,15 +3,19 @@ package com.example.administrator.dqd;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class FeedbackActivity extends Activity{
 
     private ImageView title_back;
+    private LinearLayout linearLayout;
+    private float  mPosX, mPosY,mCurPosX,mCurPosY;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +45,51 @@ public class FeedbackActivity extends Activity{
             //!!!是设置ContentView的第一个子view的FitsSystemWindow,使其不为系统View预留空间
             ViewCompat.setFitsSystemWindows(mChildView,false);
         }
+        //此方法可以监听滑动，实现右滑退出，但是无法检测速度来判定是否启动退出
+        setGestureListener();
+
     }
+
+    private void setGestureListener(){
+
+        linearLayout = findViewById(R.id.feedback_linearLayout);
+
+        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        mPosX = event.getX();
+                        mPosY = event.getY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        mCurPosX = event.getX();
+                        mCurPosY = event.getY();
+
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if (mCurPosX - mPosX > 0
+                                && (Math.abs(mCurPosX - mPosX) > 200)) {
+                            //向右滑动
+
+                            if((Math.abs(mCurPosY - mPosY) < 200)){
+                                finish();
+                            }
+                        } else if (mCurPosX - mPosX < 0
+                                && (Math.abs(mCurPosX - mPosX) > 25)) {
+                            //向左滑动
+                        }
+
+                        break;
+                }
+                return true;
+            }
+
+        });
+    }
+
 
 }
--- app/src/main/java/com/example/administrator/dqd/NoticeActivity.java
+++ app/src/main/java/com/example/administrator/dqd/NoticeActivity.java
@@ -3,15 +3,19 @@ package com.example.administrator.dqd;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class NoticeActivity extends Activity {
 
     private ImageView title_back;
+    private LinearLayout linearLayout;
+    private float  mPosX, mPosY,mCurPosX,mCurPosY;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,5 +46,50 @@ public class NoticeActivity extends Activity {
             ViewCompat.setFitsSystemWindows(mChildView,false);
         }
 
+        //此方法可以监听滑动，实现右滑退出，但是无法检测速度来判定是否启动退出
+        setGestureListener();
+
     }
+
+    private void setGestureListener(){
+
+        linearLayout = findViewById(R.id.notice_linearLayout);
+
+        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        mPosX = event.getX();
+                        mPosY = event.getY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        mCurPosX = event.getX();
+                        mCurPosY = event.getY();
+
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if (mCurPosX - mPosX > 0
+                                && (Math.abs(mCurPosX - mPosX) > 200)) {
+                            //向右滑动
+
+                            if((Math.abs(mCurPosY - mPosY) < 200)){
+                                finish();
+                            }
+                        } else if (mCurPosX - mPosX < 0
+                                && (Math.abs(mCurPosX - mPosX) > 25)) {
+                            //向左滑动
+                        }
+
+                        break;
+                }
+                return true;
+            }
+
+        });
+    }
+
 }
